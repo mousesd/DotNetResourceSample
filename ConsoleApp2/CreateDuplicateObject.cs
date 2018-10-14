39e48c9ff387e2ff6307bdf91d0d0011d9827568
Lee SangDae <mousesd@gmail.com>
2018-10-14 23:46:37 +0900

    Item2

---
+++ ConsoleApp2/CreateDuplicateObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace ConsoleApp2
+{
+    #region == Question ==
+    public class CreateDuplicateObject
+    {
+        private List<string> labels = new List<string>();
+
+        public CreateDuplicateObject() { }
+
+        public CreateDuplicateObject(int capacity)
+        {
+            labels = new List<string>(capacity);
+        }
+    }
+    #endregion
+
+    #region == Answer ==
+    public class CreateDuplicateObjectEx
+    {
+        private List<string> labels;
+
+        public CreateDuplicateObjectEx()
+        {
+            labels = new List<string>();
+        }
+
+        public CreateDuplicateObjectEx(int capacity)
+        {
+            labels = new List<string>(capacity);
+        }
+    } 
+    #endregion
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //# 1.Background
+            //# 1) 멤버 초기화 구문의 실행 순서
+
+            //# 2.멤버 초기화 구문
+
+            //# 3.할당 구문
+            //# 객체를 0 또는 1로 초기화 하는 경우
+            //# CreateDuplicateObject: 동일객체를 반복해서 초기화 하는 경우
+        }
+    }
+}
