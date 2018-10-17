c5ed7c0069b69decfb76aa74ebb79d76040cfa61
Lee Sangdae <mousesd@gmail.com>
2018-10-17 15:25:40 +0900

    Item #13, #14, #15, #16

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region == 1.생성자 체인 ==
+            //# Question) 문제점은?
+            //# 1) 
+            //# 2) 
+            //var product = new Product("PHONE_01", "iPhone6s", new DateTime(2014, 1, 1));
+            //Console.WriteLine(product);
+            #endregion
+
+            #region == 2.기본값 파라메터를 사용하는 생성자 ==
+            //# Question) 문제점은?
+            var product = new ProductWithDefaultValueParams(name: "iPhone6s");
+            #endregion
+
+            #region == 3.Builder pattern ==
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp4/VirtualSample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public class Base
+    {
+        protected Base()
+        {
+            this.PrintMessage();
+        }
+
+        public virtual void PrintMessage()
+        {
+            Console.WriteLine("VirtualMethod in Base class");
+        }
+    }
+
+    public class Derived : Base
+    {
+        private readonly string message = "Readonly message";
+        public Derived(string message)
+        {
+            this.message = message;
+        }
+
+        public override void PrintMessage()
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
